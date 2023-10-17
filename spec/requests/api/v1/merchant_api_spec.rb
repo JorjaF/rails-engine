2d39f7f626842aa7f2d0c7e8265347c2be76066d
Jorja Fleming <aset284@gmail.com>
2023-10-17 09:19:42 -0600

    started

---
+++ spec/requests/api/v1/merchant_api_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+# RSpec.describe "Merchant API", type: :request do
+#   before :each do
+#     @merchant_1 = Merchant.create!(name: "Merchant 1")
+#     @merchant_2 = Merchant.create!(name: "Merchant 2")
+#   end
+# end
